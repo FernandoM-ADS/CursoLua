6b5cbb60f3ebf158e90a459854f50fc4b6583178
Fernando Mauricio <fernandomauricio.ads@gmail.com>
2023-01-02 08:29:14 -0300

    first commit

---
+++ Lua_01.lua
@@ -0,0 +1,39 @@
+string = 'teste'
+string2 = "aspas duplas"
+string3 = [[
+    teste com quebra de linha
+]]
+
+numero = 50
+
+bool = string4
+
+if bool ~= nil or numero ~= 50 then
+    print('caiu aqui')
+elseif numero then
+    print('numero existe:', numero)
+else
+    print('variaveis não são verdadeiras')
+end
+
+while bool == nil do
+    print(bool)
+    if numero == 50 then
+       bool = true
+    end
+    print(bool)
+end
+
+repeat
+    print(bool)
+    if numero == 50 then
+        bool = true
+    end
+until bool
+
+for 1=10, 1, -1 do
+    print(i)
+end
+
+function soma(x, y)
+    return x+y
